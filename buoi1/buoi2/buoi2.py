567d97533f63dd721564f7f3927c5929e758b6fe
tranchii <tranchi7428@gmail.com>
2024-07-15 22:10:27 +0700

    bttvn

---
+++ buoi1/buoi2/buoi2.py
@@ -0,0 +1,28 @@
+#a= "mystring"
+#b=5
+#print(a+str(b))
+#print(a)
+#print(a[0])
+#print(a[0:6])
+#sliced_string = a[0:6]
+#print("len(sliced_string)=",len(sliced_string))
+# print(type(a),a, sep="\n")
+#
+#a=" i love python"
+#print("truoc khi thay doi",a)
+#a[0]='Y'
+#print("sau khi thay doi",a)
+#my_list= [1,2,3 ,'helo']
+#print ("my_list", my_list)
+#print ("Type:", type(my_list))
+#print("my_list[0]",my_list[0])
+#print("my_list[1:3]=", my_list[1:3])
+#print("my_list[1:4]",my_list[1:4])
+# toan tu nhan dang
+#a=5
+#b=6
+# if<expr> :
+#<statement> doan code chay khi thụt lề
+for i in range(0,10,1):
+    print(i, end='')
+    
