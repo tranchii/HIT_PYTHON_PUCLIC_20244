6c50176d1d480d2cbe932ffb53581b26031d96cc
tranchii <tranchi7428@gmail.com>
2024-08-26 19:48:33 +0700

    ktra

--- buoi1/bài2.py
+++ buoi1/bài2.py
@@ -1,5 +1,16 @@
-n = input("Nhap chuoi: ").strip()
-my_dict = {}
-for i in n:
-    my_dict[i] = my_dict.get(i, n.count(i))
-print(my_dict)
+k=int(input("nhập k: "))
+n=0
+for i in range(1,500):
+    sum=0 
+    a=i
+    while i>0:
+         sum+=i%10
+         i=i//10
+    if(sum==10):
+         n+=1
+    if(n==k):
+         print(a)
+         break
+
+
+    
--- buoi1/bài3.py
+++ buoi1/bài3.py
@@ -1,9 +1,32 @@
-import random
-n = int(input("Nhap n: "))
-l = ["CNTT", "KHMT", "KTPM", "HTTT"]
-my_dict = dict()
-for i in range(n):
-    my_dict[f"Account{i}"]= {
-        "Username": + 20236000 + i + 1 , "Password": random.choice(l) + str(20236000 + 1 + i)
-    }
-print(my_dict)
+class Vehicle:
+    def __init__(self, make):
+        self.make = make
+
+    def description(self):
+        return f"Vehicle Make: {self.make}"
+
+class Car(Vehicle):
+    def __init__(self, make, model):
+        super().__init__(make)
+        self.model = model
+
+    def description(self):
+        return f"Car Make: {self.make}, Model: {self.model}"
+
+class ElectricCar(Car):
+    def __init__(self, make, model, battery_size):
+        super().__init__(make, model)
+        self.battery_size = battery_size
+
+    def description(self):
+        return f"ElectricCar Make: {self.make}, Model: {self.model}, Battery Size: {self.battery_size} kWh"
+
+
+vehicle = Vehicle("Toyota")
+car = Car("Honda", "Civic")
+electric_car = ElectricCar("Tesla", "Model S", 100)
+
+print(vehicle.description())
+print(car.description())
+print(electric_car.description())
+
